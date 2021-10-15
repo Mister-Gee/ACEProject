5ce036f22592b8a38f5ad3ad081542f72962f2bd
fakuade gbenga <fakuadegbenga@gmail.com>
2021-10-15 10:14:56 +0100

    First Commit

---
+++ ACE.Domain/Abstract/GenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACE.Domain.Abstract
+{
+    public abstract class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+    }
+}
---
+++ ACE.Domain/Abstract/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace ACE.Domain.Abstract
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Remove(T entity);
+
+        void Dispose();
+
+        void FindAll(Expression<Func<T, bool>> predicate);
+
+        bool Save();
+    }
+}
